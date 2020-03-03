4a767c39bd2c1bf068323b625fd19621de7f6d69
ZavbakGit <f%jtVPW3hbmjy!>
2020-03-03 12:06:45 +1000

    add getListEntity

--- app/src/main/java/fun/gladkikh/cargologistic/domain/repository/MainRepository.kt
+++ app/src/main/java/fun/gladkikh/cargologistic/domain/repository/MainRepository.kt
@@ -2,11 +2,15 @@ package `fun`.gladkikh.cargologistic.domain.repository
 
 import `fun`.gladkikh.cargologistic.common.type.Either
 import `fun`.gladkikh.cargologistic.common.type.Failure
+import `fun`.gladkikh.cargologistic.domain.entity.PrintEntity
 import `fun`.gladkikh.cargologistic.domain.entity.ProductEntity
+import androidx.lifecycle.LiveData
 import java.util.*
 
 interface MainRepository {
     fun getProductByBarcode(barcode: String): Either<Failure, ProductEntity>
+
+    fun getListPrintEntity():LiveData<PrintEntity>
 
     fun printLabel(
         guidPrinter: String,
