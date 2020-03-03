23b0307c713df38602f5ccee4f69629be48b126a
ZavbakGit <f%jtVPW3hbmjy!>
2020-03-03 11:47:22 +1000

    add main repository

--- app/src/main/java/fun/gladkikh/cargologistic/domain/repository/MainRepository.kt
+++ app/src/main/java/fun/gladkikh/cargologistic/domain/repository/MainRepository.kt
@@ -1,19 +1,11 @@
 package `fun`.gladkikh.cargologistic.domain.repository
 
-
 import `fun`.gladkikh.cargologistic.common.type.Either
 import `fun`.gladkikh.cargologistic.common.type.Failure
-import `fun`.gladkikh.cargologistic.common.type.None
-import `fun`.gladkikh.cargologistic.domain.entity.AccountEntity
 import `fun`.gladkikh.cargologistic.domain.entity.ProductEntity
 import java.util.*
 
-interface AccountRepository {
-    fun getAccountEntity(): Either<Failure, AccountEntity>
-    fun saveAccountEntity(account: AccountEntity): Either<Failure, None>
-    fun login(password: String): Either<Failure, AccountEntity>
-    fun testRemoteRequest(): Either<Failure, String>
-
+interface MainRepository {
     fun getProductByBarcode(barcode: String): Either<Failure, ProductEntity>
 
     fun printLabel(
